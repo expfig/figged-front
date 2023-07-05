24f6a1828b1dedef8868aff14acfe13adee7a51d
Weverson-Luan <wltech2022@gmail.com>
2023-07-05 09:44:07 -0300

    feat ✨ criação de função para retornar informando se tem informação em uma variável ou não

---
+++ src/utils/return-text.ts
@@ -0,0 +1,15 @@
+/**
+ * IMPORTS
+ */
+
+const hanfleReturnText = (text: string) => {
+	if (text) {
+		return text;
+	}
+	return "não informado";
+};
+/**
+ * EXPORTS
+ */
+
+export { hanfleReturnText };
