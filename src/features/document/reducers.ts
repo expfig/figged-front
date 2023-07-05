058f390b828934179088f0b4a44bde34a5901212
Weverson-Luan <wltech2022@gmail.com>
2023-07-05 13:42:04 -0300

    feat :sparkles: criação do REDUCER para o documento

---
+++ src/features/document/reducers.ts
@@ -0,0 +1,32 @@
+/**
+ * IMPORTS
+ */
+import { type ActionReducerMapBuilder } from "@reduxjs/toolkit";
+
+// actions
+import * as actions from "./actions";
+
+// typings
+import { type IDocumentState } from "./interface";
+
+/**
+ * Eu sou um documento redutor.
+ */
+const document = (state: any, action: any) => {
+	// set state
+	state.document = action?.payload?.data.data;
+};
+
+/**
+ * Eu construo um redutor extra para documento.
+ */
+const buildDocument = (
+	builder: ActionReducerMapBuilder<IDocumentState>
+): void => {
+	builder.addCase(actions.fetchAllDocuments.fulfilled, document);
+};
+
+/**
+ * EXPORTS
+ */
+export { buildDocument };
