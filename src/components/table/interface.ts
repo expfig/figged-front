339d147f152ae28ae295117d798584a639440b4a
Weverson-Luan <wltech2022@gmail.com>
2023-07-05 09:48:18 -0300

    fix 🐛 correção no componente tabela com todas as propiedades e dados necessários que o componete precisa para funcionar corretamente

--- src/components/table/interface.ts
+++ src/components/table/interface.ts
@@ -7,10 +7,42 @@ interface WrapperTextFooterProps {
 }
 
 interface TextNumberPageProps {
-	color?: string;
+	active?: boolean;
+}
+
+interface IApprovalRequest {
+	id: string | number;
+	status: string;
+	coil_number: string;
+	trip_number: string;
+	type: string;
+	driver_name: string;
+	placa: string;
+	group_name: string;
+	created_at: string;
+	created_at_formatted: string;
+}
+
+interface IDataTableProps {
+	data: IApprovalRequest[];
+	pages: any[];
+	onClickPreview: (page?: number | string) => void;
+	onClickNext: (page?: number | string) => void;
+}
+
+interface IDataPagesProps {
+	url: string;
+	label: string;
+	active: boolean;
 }
 
 /**
  * EXPORTS
  */
-export type { WrapperTextFooterProps, TextNumberPageProps };
+export type {
+	WrapperTextFooterProps,
+	TextNumberPageProps,
+	IDataTableProps,
+	IApprovalRequest,
+	IDataPagesProps,
+};
