6bd402107749d0207de8b050218fd1b6f4d1f423
Weverson-Luan <wltech2022@gmail.com>
2023-07-05 12:28:47 -0300

    feat :sparkles: criação das ACTIONS para o documento

--- src/features/document/actions-types.ts
+++ src/features/document/actions-types.ts
@@ -4,6 +4,14 @@
 import { type IDocumentState, NAME } from "./interface";
 
 /**
+ * Eu sou a tipagem da requisição da actions.
+ */
+export interface DataProps {
+	idAprovacao: number;
+	token: string;
+}
+
+/**
  * Document response interface.
  */
 export type IDocumentDataResponse = IDocumentState;
