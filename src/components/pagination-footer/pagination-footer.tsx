77420ae4d5ed5325fc14dfd719a5c35e970a9a22
Weverson-Luan <wltech2022@gmail.com>
2023-07-13 14:23:05 -0300

    feat ✨ criação do componente (pagination-footer)

--- src/components/pagination-footer/pagination-footer.tsx
+++ src/components/pagination-footer/pagination-footer.tsx
@@ -15,47 +15,69 @@ import {
 	WrapperTextFooter,
 	TextNumberPage,
 } from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { type IDataPagesProps } from "./interface";
 import { FiArrowRight, FiArrowLeft } from "react-icons/fi";
-import { dataFake } from "./data-fake";
 
-const PaginationFooter = () => {
+const PaginationFooter = ({
+	pageData,
+	firstPage,
+	lastpage,
+	onClickNext,
+	onClickPreview,
+	isLoadingPagination,
+}: any) => {
 	const theme = useTheme();
-	const [data] = useState<any[]>([1, 2, 3, 4, 5, 7, 8, 9, 10]);
-	const [pages] = useState<any[]>(dataFake);
+
+	const [pages, setPages] = useState<IDataPagesProps[]>([]);
+
+	useEffect(() => {
+		setPages(pageData);
+	}, [pageData]);
 
 	return (
 		<Container>
 			<>
-				{data.length ? (
-					<FooterTable>
-						<ButtonPreview onClick={() => {}}>
-							<FiArrowLeft size={18} color={theme.colors.natural} />
-							<TextSpanLeft>Anterior</TextSpanLeft>
-						</ButtonPreview>
-
-						{pages?.map((page: IDataPagesProps) => (
-							<WrapperTextFooter
-								key={page?.label}
-								background={page?.active}
-								onClick={() => {}}
-							>
-								<TextNumberPage active={page?.active}>
-									{page?.label}
-								</TextNumberPage>
-							</WrapperTextFooter>
-						))}
-
-						<ButtonNext onClick={() => {}}>
-							<TextSpanRight>Próximo</TextSpanRight>
-							<FiArrowRight size={18} color={theme.colors.natural} />
-						</ButtonNext>
-					</FooterTable>
+				{isLoadingPagination ? (
+					<p>carrregando..</p>
 				) : (
-					<WrapperTextNoData>
-						<p>Nenhuma informação encontrada....</p>
-					</WrapperTextNoData>
+					<>
+						{pages.length ? (
+							<FooterTable>
+								{pages.length > 1 && (
+									<ButtonPreview onClick={onClickPreview}>
+										<FiArrowLeft size={18} color={theme.colors.natural} />
+										<TextSpanLeft>Anterior</TextSpanLeft>
+									</ButtonPreview>
+								)}
+
+								{pages?.map((page: IDataPagesProps) => (
+									<WrapperTextFooter
+										key={page?.label}
+										background={page?.active}
+										onClick={() => onClickNext(page?.label)}
+									>
+										<TextNumberPage active={page?.active}>
+											{page?.label}
+										</TextNumberPage>
+									</WrapperTextFooter>
+								))}
+
+								{firstPage === lastpage ? (
+									<></>
+								) : (
+									<ButtonNext onClick={onClickNext}>
+										<TextSpanRight>Próximo</TextSpanRight>
+										<FiArrowRight size={18} color={theme.colors.natural} />
+									</ButtonNext>
+								)}
+							</FooterTable>
+						) : (
+							<WrapperTextNoData>
+								<p>Nenhuma informação encontrada....</p>
+							</WrapperTextNoData>
+						)}
+					</>
 				)}
 			</>
 		</Container>
